5d59477d8dfb02bc3ac87a61928a0966ffa7fa0a
unknown <you@example.com>
2024-02-10 14:41:14 -0300

    Recurso para criar um novo Usuário

--- src/main/java/com/fabiomelo/parkapi/service/UsuarioService.java
+++ src/main/java/com/fabiomelo/parkapi/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.fabiomelo.parkapi.service;
 
+import com.fabiomelo.parkapi.entity.Usuario;
 import com.fabiomelo.parkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +13,8 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
 
 
-
-
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
